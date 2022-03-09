5980bba0422c5da4de5fe7cbcbddbdda4da5f4c4
hyeseungS <hyeseung2000@gmail.com>
2022-03-09 16:22:55 +0900

    Initial commit

---
+++ src/main/java/com/hodumaru/newsmaru/NewsmaruApplication.java
@@ -0,0 +1,13 @@
+package com.hodumaru.newsmaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsmaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsmaruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodumaru/newsmaru/NewsmaruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodumaru.newsmaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsmaruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
