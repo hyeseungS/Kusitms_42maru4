6a593dae8d9b1cb0caf82672dfcfed0447ab9a4a
jihyehann <haneh0714@gmail.com>
2022-03-11 22:19:43 +0900

    Add: 뉴스 보기 - 뉴스 목록 조회

--- src/main/java/com/hodumaru/newsmaru/model/Article.java
+++ src/main/java/com/hodumaru/newsmaru/model/Article.java
@@ -21,7 +21,7 @@ public class Article extends Timestamped {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 2000)
     private String content;
 
     @Column(nullable = false)
