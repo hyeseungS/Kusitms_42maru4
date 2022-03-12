0ac685de4aae0dd4c6fd40621a9fbbbc879696ef
hyeseungS <hyeseung2000@gmail.com>
2022-03-12 13:17:31 +0900

    Update : MY 뉴스 정렬 최신순

--- src/main/java/com/hodumaru/newsmaru/controller/MyNewsController.java
+++ src/main/java/com/hodumaru/newsmaru/controller/MyNewsController.java
@@ -2,7 +2,7 @@ package com.hodumaru.newsmaru.controller;
 
 import com.hodumaru.newsmaru.model.Article;
 import com.hodumaru.newsmaru.security.UserDetailsImpl;
-import com.hodumaru.newsmaru.service.MynewsService;
+import com.hodumaru.newsmaru.service.MyNewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -13,15 +13,14 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class MynewsController {
+public class MyNewsController {
 
-    private final MynewsService mynewsService;
+    private final MyNewsService mynewsService;
 
     // MY 뉴스 페이지
     @GetMapping("/mynews")
     public String getMynews(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("username", userDetails.getUsername());
-
         List<Article> viewList = mynewsService.getViewArticles(userDetails.getUser());
         model.addAttribute("views", viewList);
         List<Article> clipList = mynewsService.getClipArticles(userDetails.getUser());
--- src/main/java/com/hodumaru/newsmaru/service/MyNewsService.java
+++ src/main/java/com/hodumaru/newsmaru/service/MyNewsService.java
@@ -6,12 +6,8 @@ import com.hodumaru.newsmaru.model.User;
 import com.hodumaru.newsmaru.model.View;
 import com.hodumaru.newsmaru.repository.ArticleRepository;
 import com.hodumaru.newsmaru.repository.ClipRepository;
-import com.hodumaru.newsmaru.repository.UserRepository;
 import com.hodumaru.newsmaru.repository.ViewRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +16,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class MynewsService {
+public class MyNewsService {
 
     private final ArticleRepository articleRepository;
     private final ViewRepository viewRepository;
@@ -31,7 +27,8 @@ public class MynewsService {
             User user
     ) {
         Long userId = user.getId();
-        List<View> viewList = viewRepository.findAllByUserId(userId);
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        List<View> viewList = viewRepository.findAllByUserId(userId, sort);
         List<Article> articleList = new ArrayList<>();
         for(View view : viewList) {
             Article article = articleRepository.findById(view.getArticle().getId())
@@ -46,7 +43,9 @@ public class MynewsService {
             User user
     ) {
         Long userId = user.getId();
-        List<Clip> clipList = clipRepository.findAllByUserId(userId);
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        List<Clip> clipList = clipRepository.findAllByUserId(userId, sort);
+
         List<Article> articleList = new ArrayList<>();
         for(Clip clip : clipList) {
             Article article = articleRepository.findById(clip.getArticle().getId())
